1e9db8faedae0f8e639d5971ae7894e6f7c657a5
DanieleBernassola <danielebernassola02@gmail.com>
2024-06-03 16:59:33 +0200

    [UPD] Added font montserrat

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script></script>
 <template>
-  <h1>Vue Vite Template</h1>
+  <h1>Vite Rick and Morty</h1>
   <p>Template per progetti Vue con Vite</p>
 </template>
 <style></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 // import style
 import "./assets/scss/main.scss";
+// import font
+import "@fontsource/montserrat/400.css";
 // import vue root component
 import App from "./App.vue";
 
